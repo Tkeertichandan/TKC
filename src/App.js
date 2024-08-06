54842ed7629cea7c45fae51c4a57f2ee0b0d5c0f
Tkeertichandan <2300033901@kluniversity.in>
2024-08-06 06:34:43 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Faculty from './components/Faculty';
+import Student from './components/Student';
+import Courses from './components/Courses';
+import Component from './components/Component';
+import User from './components/User';
+import Assignment from './components/Assignment';
+
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Footer/>
+      <Faculty/>
+      <Student/>
+      <Courses/>
+      <Component/>
+      <User/>
+      <Assignment/>
+     
+      
+       
+   
+      
+  
+     
+      
+      
+
     </div>
+    
   );
+
 }
 
 export default App;
